5ccdc8fe036bb8baf9ad9df01682e52b38b3f460
Valinsky-Pimentel <valpimentel1019@gmail.com>
2022-11-05 07:16:18 -0600

    Create ejemplo10.js

---
+++ JAVA-PRIMERAPARTE/Actividad-10/ejemplo10.js
@@ -0,0 +1,8 @@
+function onKeyUp(event){
+    console.log('onKeyUp:'+event.key);
+}
+document.addEventListener('keyup', onKeyUp);
+var article = document.getElementById('electriccars');
+article.dataset.columns
+article.dataset.indexNumver
+article.dataset.parent
