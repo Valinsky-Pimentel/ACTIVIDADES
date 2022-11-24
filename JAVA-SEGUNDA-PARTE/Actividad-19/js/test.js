3f410ba5478e0b1a5cde9a525b2b6a050c8cdfc3
Valinsky-Pimentel <valpimentel1019@gmail.com>
2022-11-23 23:51:11 -0600

    Create test.js

---
+++ JAVA-SEGUNDA-PARTE/Actividad-19/js/test.js
@@ -0,0 +1,16 @@
+const TEST_PIELES = {
+  'La textura de tu piel es ':['Delgada','Semigruesa','Gruesa'],
+  'Has notado que tus poros estan ':['Abiertos','Semiabiertos','Cerrados'],
+  'Como ves la apariencia de tu cutis ':['Opaca','Sonrojada','Brillante'],
+  'Has notado que tienes espinillas en la zona de expresion ':['si','no'],
+  'has notado tu piel deshidratada o tu piel como cascara de naranja':['si','no']
+}
+
+
+const TEST_RESPUESTAS = {
+  '11111':'pielacneica.html',
+  '22222':'pielgrasa.html',
+  '33322':'pielmixta.html',
+  '12121':'pielnormal.html',
+  '11122':'pielseca.html'
+}
