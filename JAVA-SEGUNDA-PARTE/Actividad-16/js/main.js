78dc1cacf00e3a4eb97aa2c2a8c3fac253b9943b
Valinsky-Pimentel <valpimentel1019@gmail.com>
2022-11-29 12:38:38 -0600

    Create main.js

---
+++ JAVA-SEGUNDA-PARTE/Actividad-16/js/main.js
@@ -0,0 +1,4 @@
+const albumview = document.querySelector('#album-view')
+const album22 = document.querySelector('#album-view2')
+const album1 = new Album(albumview, PHOTO_LIST)
+const album2 = new Album(album22, REGALOS_ENLACES)
