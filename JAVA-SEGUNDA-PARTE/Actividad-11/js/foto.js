73e55966444e234b191c229c0e7af764fc3502e4
Valinsky-Pimentel <valpimentel1019@gmail.com>
2022-11-29 12:39:51 -0600

    Update foto.js

--- JAVA-SEGUNDA-PARTE/Actividad-11/js/foto.js
+++ JAVA-SEGUNDA-PARTE/Actividad-11/js/foto.js
@@ -41,26 +41,44 @@ const modalView = document.querySelector('#modal-view');
 modalView.addEventListener('click', onModalClick);
 
 
+/*
+Realiza acciones dependiendo de la tecla presionada
+Parametro: Evento
+*/
 function nextPhoto(event) {
-    if (event.key === 'Escape') {
-        hideModal();
-        return;
-    }
-    if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
-        return;
-    }
-    let nextIndex = currentIndex;
-    if (event.key === 'ArrowLeft') {
-        nextIndex--;
-    } else {
-        nextIndex++;
-    }
-    if (nextIndex < 0 || nextIndex == PHOTO_LIST.length) {
-        return;
-    }
-    const photoSrc = PHOTO_LIST[nextIndex];
-    modalView.innerHTML = '';
-    const image = createImage(photoSrc);
-    modalView.appendChild(image);
-    currentIndex = nextIndex;
+// Si la tecla presionada fue ESCAPE, ocultamos el dialogo modal
+if (event.key === 'Escape') {
+hideModal();
+return;
+}
+//Si la tecla es diferente de flacha izquierda o derecha hacemos nada
+if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
+return;
+}
+//Guardamos el valor del indice actual
+let nextIndex = currentIndex;
+if (event.key === 'ArrowLeft') {
+// Si la tecla presionada es la flecha izquierda decrementamos el
+indice
+nextIndex--;
+} else {
+// Si la tecla presionada no es la flecha izquierda incrementamos el
+indice
+nextIndex++;
+}
+
+//Si hemos sobrepasado los limites del arreglo, hacer nada
+if (nextIndex < 0 || nextIndex == PHOTO_LIST.length) {
+return;
+}
+//asignar una imagen del arreglo de fotos con el nuevo indice
+const photoSrc = PHOTO_LIST[nextIndex];
+//eliminar el contenido actual de la vista
+modalView.innerHTML = '';
+//crear el nuevo elemento imagen
+const image = createImage(photoSrc);
+//adjuntar la imagen a la vista
+modalView.appendChild(image);
+//actualizar el valor del indice actual
+currentIndex = nextIndex;
 }
